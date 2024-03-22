b98617a2001fc629a4bc063f8f5f1acea69661d2
Don-Isdale <Don.Isdale@gmail.com>
2024-03-22 04:35:10 +0000

    updated auto update script to work for amazon deployment

--- tools/automated-pull-from-github.bash
+++ tools/automated-pull-from-github.bash
@@ -1,6 +1,10 @@
 source ~/.bash_custom
 # which source-s other files including ~/scripts/functions_hosted.bash
 
+if [ -d "~AGG_pretzel/AGG-pretzel-documentation-website/tools/logs" ];
+    then mkdir -p ~/AGG-pretzel-documentation-website/tools/logs
+fi
+
 eval "$(ssh-agent)"
-ssh-add ~/.ssh/github
-cd AGG-pretzel-documentation-website/ && git pull
+ssh-add ~/.ssh/keys/github
+cd ~/AGG_pretzel/AGG-pretzel-documentation-website/ && git pull
